b75f7fc9230a09268bd5e0342f50d4226d9b7f9c
Samuel Vacca <svacca@pointdynamics.com>
2018-02-26 22:36:45 -0500

    move components into common module

--- packages/voter/src/pages/ballot-results/ballot-results.module.ts
+++ packages/voter/src/pages/ballot-results/ballot-results.module.ts
@@ -2,19 +2,15 @@ import {NgModule} from '@angular/core';
 import {IonicPageModule} from 'ionic-angular';
 import {BallotResultsPage} from './ballot-results';
 import {TranslateModule} from "@ngx-translate/core";
-import {ResultsChartComponent} from "../../components/results-chart/results-chart";
-
 import {ComponentsModule} from '../../components/components.module';
-
 @NgModule({
   declarations: [
-    BallotResultsPage,
-    ResultsChartComponent,
-    ComponentsModule
+    BallotResultsPage
   ],
   imports: [
     IonicPageModule.forChild(BallotResultsPage),
-    TranslateModule.forChild()
+    TranslateModule.forChild(),
+    ComponentsModule
   ],
 })
 export class BallotResultsPageModule {
