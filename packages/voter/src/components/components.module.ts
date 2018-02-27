a005a5c8fb766ed67b271d90c07741b4cbc50a81
Samuel Vacca <svacca@pointdynamics.com>
2018-02-26 22:36:45 -0500

    move components into common module

--- packages/voter/src/components/components.module.ts
+++ packages/voter/src/components/components.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
+import {IonicPageModule} from 'ionic-angular';
+import {TranslateModule} from "@ngx-translate/core";
 import { ExpandableComponent } from './expandable/expandable';
 import {SpinnerComponent} from "../components/spinner/spinner";
-
+import {ResultsChartComponent} from "../components/results-chart/results-chart";
+import {TutorialComponent} from '../components/tutorial/tutorial';
 @NgModule({
 	declarations: [
 		ExpandableComponent,
-		SpinnerComponent
+		SpinnerComponent,
+		ResultsChartComponent,
+		TutorialComponent
 	],
-	imports: [],
+	imports: [
+		IonicPageModule, 
+		TranslateModule.forChild()
+	],
 	exports: [
 		ExpandableComponent,
-		SpinnerComponent
+		SpinnerComponent,
+		ResultsChartComponent,
+		TutorialComponent
 	]
 
 })
