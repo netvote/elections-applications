cc19f7dca38e07574702da17353d2ab18f82baf5
Ben Howerton <bhowerton@pointdynamics.com>
2018-03-07 22:33:33 -0500

    external auth

--- packages/voter/src/pages/ballot-info-modal/ballot-info-modal.ts
+++ packages/voter/src/pages/ballot-info-modal/ballot-info-modal.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavParams, NavController, ViewController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, NavParams, NavController, ViewController} from 'ionic-angular';
 import {TranslateService} from '@ngx-translate/core';
-import { InAppBrowser } from '@ionic-native/in-app-browser';
+import {InAppBrowser} from '@ionic-native/in-app-browser';
 
 @IonicPage()
 @Component({
@@ -19,49 +19,49 @@ export class BallotInfoModalPage {
     public navParams: NavParams,
     public translateService: TranslateService,
     private iab: InAppBrowser) {
+  }
+
+  ionViewWillLoad() {
+
+    this.ballotData = this.navParams.get('data');
+
+  }
+
+  async closeBallotInfoModal() {
+    this.viewCtrl.dismiss();
+  }
+
+  // goToResults() {
+  //   this.viewCtrl.dismiss();
+  //   this.navCtrl.setRoot("ballot-results");
+  // }
+
+  matchStatusClass(status, waiting) {
+
+    if (status === 'submitted' && waiting) {
+      return 'is-positive'
     }
-
-    ionViewWillLoad() {
-    
-      this.ballotData = this.navParams.get('data');
-
+    else if (status === 'submitted') {
+      return 'is-attention'
     }
-
-    async closeBallotInfoModal(){
-      this.viewCtrl.dismiss();
+    else {
+      return 'is-' + status;
     }
+  }
 
-    // goToResults() {
-    //   this.viewCtrl.dismiss();
-    //   this.navCtrl.setRoot("ballot-results");
-    // }
+  launchTxLink(url: string) {
 
-    matchStatusClass(status, waiting){
+    let iabDoneText: string;
 
-      if(status === 'submitted' && waiting){
-        return 'is-positive'
-      }
-      else if(status === 'submitted'){
-        return 'is-attention'
-      }
-      else{
-        return 'is-'+status;
-      }
-    }
+    this.translateService.get(['IAB_DONE_BUTTON_TEXT']).subscribe(values => {
+      iabDoneText = values.IAB_DONE_BUTTON_TEXT;
+    });
 
-    launchTxLink(url: string){
+    const iabOptions = "location=yes,clearcache=yes,transitionstyle=crossdissolve,toolbarcolor=#071822,closebuttoncolor=#5BC0BE,closebuttoncaption=" + iabDoneText;
 
-      let iabDoneText: string;
+    const browser = this.iab.create(url, '_blank', iabOptions);
 
-      this.translateService.get(['IAB_DONE_BUTTON_TEXT']).subscribe(values => {
-        iabDoneText = values.IAB_DONE_BUTTON_TEXT;
-      });
+    browser.show();
 
-      const iabOptions = "location=yes,clearcache=yes,transitionstyle=crossdissolve,toolbarcolor=#071822,closebuttoncolor=#5BC0BE,closebuttoncaption=" + iabDoneText;
-
-      const browser = this.iab.create(url, '_blank', iabOptions);
-
-      browser.show();
-
-    }
+  }
 }
