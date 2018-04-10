dd5e2da164ebdaacbdd3481cb54e7633ab98e03c
Ben Howerton <bhowerton@pointdynamics.com>
2018-04-10 11:30:49 -0400

    refac

--- packages/builder/src/app/app.module.ts
+++ packages/builder/src/app/app.module.ts
@@ -40,7 +40,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MainLayoutComponent} from './layouts/main-layout/main-layout.component';
 import {LoginLayoutComponent} from './layouts/login-layout/login-layout.component';
 import {QRCodeModule} from 'angular2-qrcode';
-import {SpinnerModule} from 'angular-spinners';
+//import {SpinnerModule} from 'angular-spinners';
 
 const appRoutes: Routes = [
 
@@ -126,8 +126,7 @@ const appRoutes: Routes = [
     QRCodeModule,
     ReactiveFormsModule,
     DynamicFormsCoreModule.forRoot(),
-    DynamicFormsBootstrapUIModule,
-    SpinnerModule
+    DynamicFormsBootstrapUIModule
   ],
   providers: [AuthService, AuthGuard, {provide: ToastOptions, useClass: CustomToastOption}],
   bootstrap: [AppComponent]
