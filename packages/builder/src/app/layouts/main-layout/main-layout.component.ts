9f65ed14b2b9f5819a3e53b0c7fdc62f696fd8fb
Samuel Vacca <svacca@pointdynamics.com>
2018-06-07 19:41:45 -0400

    live sidebar ballot count data

--- packages/builder/src/app/layouts/main-layout/main-layout.component.ts
+++ packages/builder/src/app/layouts/main-layout/main-layout.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {BallotService} from '../../services/ballot.service';
+import {Ballot} from '@netvote/core';
 
 @Component({
   selector: 'app-main-layout',
@@ -7,6 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainLayoutComponent {
 
-  constructor() { }
+  ballots: Observable<Ballot[]>;
+  ballotCount: number;
+  buildingBallotCount: number;
+  activatedBallotCount: number
+
+  constructor( private ballotService: BallotService ) { 
+
+    this.ballots = this.ballotService.getOrgBallots();    
+  }
+
+  ngOnInit(){
+    //console.log("ballots listing : ", this.ballots);
+
+    this.ballots.subscribe(result => {
+
+      this.ballotCount = result.length;
+
+      let j = 0;
+      for (var i in result) {
+        if(result[i].status === 'building'){
+          j++;
+        }
+     }
+
+     this.buildingBallotCount = j;
+    
+     this.activatedBallotCount = 0;
+    
+    });
+  }
 
 }
